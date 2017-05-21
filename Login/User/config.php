0b551760a3038f9e32a6430d833ddacdf3d5aad9
will2therich <sgs392499@sgscol.ac.uk>
2017-05-21 01:25:16 +0100

    Updating 404 Errors on logout and login

--- Login/User/config.php
+++ Login/User/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost"; // Hostname
 $port = "3306"; // MySQL Port : Default : 3306
-$user = "root"; // Username Here
-$pass = "flareservers"; //Password Here
-$db   = "gamepaneltest"; // Database Name
+$user = "$DBNAME"; // Username Here
+$pass = "$DBPASS"; //Password Here
+$db   = "$DBNAME"; // Database Name
 $dbh  = new PDO('mysql:dbname='.$db.';host='.$host.';port='.$port,$user,$pass);
 ?>
--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 session_destroy();
 
 // Redirect to login page
-header('Location: /Login/User/index.php');
+header('Location: ./Login/User/index.php');
 exit(0);
 
 ?>
